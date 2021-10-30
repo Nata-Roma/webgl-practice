0d408657033477e452fd961c41c24b46cb771928
Natalia Romashko <sharkellgm@gmail>
2021-10-30 18:37:45 +0300

    add triangle buffer

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,26 @@ import vertexShaderSource from './vertex-shader.glsl';
 import fragmentShaderSource from './fragment-shader.glsl';
 import './style.css';
 
+
+const positions = [
+  -1, -1, 
+   1, 1, 
+   1, -1, 
+   -1, -1, 
+   1, 1, 
+   -1, 1
+];
+
+const colors = [
+  1, 0, 0, 1, 
+  0, 1, 0, 1, 
+  0, 0, 1, 1,
+  1, 0, 0, 1, 
+  0, 1, 0, 1, 
+  0, 0, 1, 1
+];
+
+
 const canvas = document.createElement('canvas');
 canvas.height = document.documentElement.clientHeight;
 canvas.width = document.documentElement.clientWidth;
@@ -13,6 +33,9 @@ window.onresize = () => {
 };
 
 const gl = canvas.getContext('webgl');
+
+const triangleBuffer = createBuffer(gl, [-1, -1, 1, 1,  1, -1, ]);
+const triangleColor = createBuffer(gl, [1, 0, 0, 1, 0, 1, 0, 1, 0, 0, 1, 1,]);
 
 function createShader(
   gl: WebGLRenderingContext,
@@ -60,36 +83,16 @@ function createProgram(
 const program = createProgram(gl, vertexShader, fragmentShader);
 
 const positionAttributeLocation = gl.getAttribLocation(program, 'a_position');
-
-const positionBuffer = gl.createBuffer();
-gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-const positions = [
-  -1, -1, 
-   1, 1, 
-   1, -1, 
-   -1, -1, 
-   1, 1, 
-   -1, 1
-];
-gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
+const positionBuffer = createBuffer(gl, positions);
 
 const colorAttributeLocation = gl.getAttribLocation(program, 'a_color');
-const colorBuffer = gl.createBuffer();
-gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
-const colors = [
-  1, 0, 0, 1, 
-  0, 1, 0, 1, 
-  0, 0, 1, 1,
-  1, 0, 0, 1, 
-  0, 1, 0, 1, 
-  0, 0, 1, 1
-];
-gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
+
+const colorBuffer = createBuffer(gl, colors);
 
 gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
 
 // очищаем canvas
-gl.clearColor(0, 0, 0, 0);
+gl.clearColor(0, 0, 0, 1);
 gl.clear(gl.COLOR_BUFFER_BIT);
 
 gl.useProgram(program);
@@ -98,8 +101,6 @@ gl.enableVertexAttribArray(colorAttributeLocation);
 gl.vertexAttribPointer(colorAttributeLocation, 4, gl.FLOAT, false, 0, 0);
 
 gl.enableVertexAttribArray(positionAttributeLocation);
-// Привязываем буфер положений
-gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
 
 // Указываем атрибуту, как получать данные от positionBuffer (ARRAY_BUFFER)
 const size = 2; // 2 компоненты на итерацию
@@ -107,14 +108,6 @@ const type = gl.FLOAT; // наши данные - 32-битные числа с 
 const normalize = false; // не нормализовать данные
 const stride = 0; // 0 = перемещаться на size * sizeof(type) каждую итерацию для получения следующего положения
 const offset = 0; // начинать с начала буфера
-gl.vertexAttribPointer(
-  positionAttributeLocation,
-  size,
-  type,
-  normalize,
-  stride,
-  offset,
-);
 
 const primitiveType = gl.TRIANGLES;
 const count = 6;
@@ -124,15 +117,34 @@ let currentAngle = 0;
 function render() {
   requestAnimationFrame(() => {
     currentAngle += 0.01;
-    gl.uniform1f(u_angle, currentAngle);
-    gl.drawArrays(primitiveType, offset, count);
+
+    for (let i = 0; i < 10; i++) {
+      gl.bindBuffer(gl.ARRAY_BUFFER, i % 2 === 0 ? triangleBuffer: positionBuffer) ;
+      gl.vertexAttribPointer(
+        positionAttributeLocation,
+        size,
+        type,
+        normalize,
+        stride,
+        offset,
+      );
+      
+      gl.uniform1f(u_angle, currentAngle+i/5);
+      gl.drawArrays(primitiveType, offset, count);
+    }
     render();
   });
 }
 
 render();
 
-// let x = m11 * _x + m12 * _y + m13 * _z + m14 * _w;
-// let y = m21 * _x + m22 * _y + m23 * _z + m24 * _w;
-// let z = m31 * _x + m32 * _y + m33 * _z + m34 * _w;
-// let w = m41 * _x + m42 * _y + m43 * _z + m44 * _w;
+function createBuffer(
+  gl:WebGLRenderingContext, 
+  arr: Array<number>, 
+  ): WebGLBuffer {
+  const buffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(arr), gl.STATIC_DRAW);
+
+  return buffer;
+}
